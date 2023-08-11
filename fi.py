b28f2306ac896ca81ff7a6e1ca9d3ea0306fd332
Barish-S <barishs28@gmail.com>
2023-08-11 14:13:30 +0530

    New

---
+++ fi.py
@@ -0,0 +1,13 @@
+'''Implement a Python function to find the longest common prefix among a list of strings. 
+Example:
+ Input: ["flower", "flow", "flight"]
+ Output: "fl”'''
+
+input1=["flower","flow","flight"]
+one=input1[0]
+two=input1[1]
+three=input1[2]
+take=""
+re=""
+      
+print(re)
